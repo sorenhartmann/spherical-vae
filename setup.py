225c2c1791b8c82b1773457e4962d9a100dad775
sorenhartmann <soren.hartmann@gmail.com>
2021-03-02 10:40:01 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Spherical Variational Autoencoders',
+    author='Caroline, Ida og Søren',
+    license='MIT',
+)
