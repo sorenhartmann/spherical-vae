fixes various bugs for using autograd with model. Kinda unstable atm with a pretend kl-term

src/distributions.py
@@ -15,7 +15,6 @@ class SphereUniform(Distribution):
     def __init__(self, dim: int, batch_shape=torch.Size(), validate_args=None):
 
         self.dim = dim
-        self._normal = Normal(0, 1)
         n = dim + 1
 
         log_surface_area = math.log(2) + n / 2 * math.log(math.pi) - math.lgamma(n / 2)
@@ -39,9 +38,10 @@ class SphereUniform(Distribution):
             else torch.Size(sample_shape)
         )
 
-        norm_sample = self._normal.sample(
+        norm_sample = torch.zeros(
             sample_shape + self._batch_shape + self._event_shape
         )
+        norm_sample.normal_()
 
         return norm_sample / norm_sample.norm(dim=-1, keepdim=True)
 
@@ -113,11 +113,12 @@ class VonMisesFisher(Distribution):
             if n_left == 0:
                 break
 
+            # TODO: probably can't train until kl term is there, k is too large 
             a_ = torch.masked_select(a, mask)
             b_ = torch.masked_select(b, mask)
             d_ = torch.masked_select(d, mask)
 
-            epsilon = beta_dist.sample((n_left,))
+            epsilon = beta_dist.rsample((n_left,))
 
             w_proposal = (1 - (1 + b_) * epsilon) / (1 - (1 - b_) * epsilon)
             t = 2 * a_ * b_ / (1 - (1 - b_) * epsilon)
@@ -125,10 +126,13 @@ class VonMisesFisher(Distribution):
             u = torch.rand(n_left)
             accepted = (m - 1) * torch.log(t) - t + d_ >= torch.log(u)
 
-            # Find values of
-            mask[mask.clone()] = accepted
-            w[mask] = w_proposal[accepted]
-            done[mask] = True
+            # Fix for mask inplace stuff with masked_select
+            mask_clone = mask.clone()
+
+            mask_clone[mask] = accepted 
+            w[mask_clone] = w_proposal[accepted]
+            done[mask_clone] = True
+
 
         w = w.view(sample_shape + batch_shape + (1,))
 

src/models/svae.py
@@ -60,7 +60,7 @@ class SphericalVAE(Module):
     def observation_model(self, z: Tensor) -> Distribution:
         """return the distribution `p(x|z)`"""
         h_z = self.decoder(z)
-        mu, log_sigma = h_z.data.chunk(2, dim=-1)
+        mu, log_sigma = h_z.chunk(2, dim=-1)
 
         return Normal(mu, log_sigma.exp())
 
@@ -82,31 +82,6 @@ class SphericalVAE(Module):
 # %% 
 if __name__ == "__main__":
 
-    X, y = genNoisySynthDataS2(plotCartesian=False, Nsamples=100)
-
-    X = torch.tensor(X, dtype=torch.float)
-    y = torch.tensor(y, dtype=torch.float)
-
-    feature_dim = X.shape[-1]
-
-    svae = SphericalVAE(feature_dim=feature_dim, latent_dim=3)
-
-    vmf = svae.posterior(X)
-    sample_shape = torch.Size([100])
-    z = vmf.rsample(sample_shape).detach().numpy()
-
-    fig = go.Figure()
-    for i in range(z.shape[-2]):
-        x, y, z_ = z[:, i, :].T
-        fig.add_trace((go.Scatter3d(
-            x=x, 
-            y=y, 
-            z=z_, 
-            mode="markers"
-        )))
-
-    output = svae(X)
-    
     # Splitting synthetic data into train and validation 
     X, y = genNoisySynthDataS2(plotCartesian=False, Nsamples=100)
 
@@ -118,7 +93,7 @@ if __name__ == "__main__":
     y_val = torch.tensor(y_val, dtype=torch.float)
     
     ## Training loop
-    n_epochs = 3
+    n_epochs = 100
     batch_size = 16
 
     n_batches_train = int(np.ceil(X_train.shape[0]/batch_size))
@@ -130,10 +105,10 @@ if __name__ == "__main__":
     validation_loss = [None]*n_epochs
     
     # Model and optimizer
+    feature_dim = X.shape[-1]
     svae = SphericalVAE(feature_dim=feature_dim, latent_dim=3)
 
     optimizer = torch.optim.Adam(svae.parameters(), lr=1e-3) 
-
     for epoch in range(n_epochs):
 
         trainloader = torch.utils.data.DataLoader(X_train, batch_size=batch_size)
@@ -147,9 +122,8 @@ if __name__ == "__main__":
             
             # Forward pass
             output = svae.forward(batch)
-            loss = -output["px"].log_prob(batch)
+            loss = -output["px"].log_prob(batch).sum() + sum(output['qz'].k) / 100 #Poor mans kl
             optimizer.zero_grad()
-            
             loss.backward()
             optimizer.step()
 
@@ -162,7 +136,7 @@ if __name__ == "__main__":
             for i, batch in enumerate(validationloader): 
                 # Forward pass
                 output = svae.forward(batch)
-                loss = -output["px"].log_prob(batch)
+                loss = -output["px"].log_prob(batch).sum() +  sum(output['qz'].k) / 100
 
                 epoch_validation_loss[i] = loss.item()
 