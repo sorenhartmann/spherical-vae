5d7605d8a4cd5163b0c465387782d8af27e59c00
Søren Hartmann <soren.hartmann@gmail.com>
2021-04-27 17:04:56 +0200

    fixed conv layers

--- src/models/cnn_encoder_decoder.py
+++ src/models/cnn_encoder_decoder.py
@@ -1,86 +1,96 @@
 import torch
-import numpy as np 
-from torch.nn import Module, Sequential, Linear, Sigmoid, ReLU,Conv2d, Dropout2d, BatchNorm2d, MaxPool2d, ConvTranspose2d
+import numpy as np
+from torch.nn import (
+    Module,
+    Sequential,
+    Linear,
+    Conv2d,
+    Dropout2d,
+    BatchNorm2d,
+    MaxPool2d,
+    ConvTranspose2d,
+)
+from torch.nn.modules.dropout import Dropout
+
 
 def compute_conv_dim(dim_size, kernel_size, padding, stride):
     return int((dim_size - kernel_size + 2 * padding) / stride + 1)
 
 def compute_conv_transpose_out_dim(in_dim, kernel_size, padding, dialation, stride):
-    return int((in_dim-1)*stride-2*padding+dialation*(kernel_size-1)+1)
-
-def compute_conv_transpose_in_dim(out_dim, kernel_size, padding, dialation, stride):
-    out_dim = (in_dim-1)*stride-2*padding+dialation*(kernel_size-1)+1
-    
-    return 
+    return int((in_dim - 1) * stride - 2 * padding + dialation * (kernel_size - 1) + 1)
 
 def compute_last_kernel_dim(out_dim_size, dim_size):
-    # For fixed stride=1, padding = 0, dialation=1 
+    # For fixed stride=1, padding = 0, dialation=1
     return int(-dim_size + out_dim_size + 1)
 
-
 class Encoder(Module):
-    # kernel_size: list of kernel sizes
-    # padding_size: list of padding sizes
-    # stride: list of strides
-    def __init__(self, image_size,  out_features, kernel_size = None, padding_size = None,
-                 out_channel_size = None, stride = None,
-                 activation_function = None, ffnn_layer_size = None,
-                 dropout = None, dropout2d = None, maxpool = None):
+    def __init__(
+        self,
+        image_size,
+        out_features,
+        kernel_size=[3, 5, 3],
+        stride=[1, 2, 2],
+        out_channel_size=[6, 9, 6],
+        padding_size=[1, 2, 1],
+        activation_function="Tanh",
+        ffnn_layer_size=[100, 1000],
+        dropout=None,
+        dropout2d=None,
+        maxpool_kernel=2,
+        maxpool_stride=2,
+    ):
 
         super().__init__()
-        
+
         self.dropout = dropout
         self.dropout2d = dropout2d
 
-        if activation_function is None:
-            activation_function = "ReLU"
         self._activation_function = activation_function
 
         ActFunc = getattr(torch.nn.modules.activation, activation_function)
 
-        if maxpool is None: 
-            maxpool = [2, 2]
-         
         # CONVOLUTIONAL LAYERS ------------------------------------------------------------------
         convs = []
         channels, height, width = image_size
-        for parameters in list(zip(out_channel_size, kernel_size, stride, padding_size)):
-            convs.append(Conv2d(
-                in_channels=channels,
-                out_channels=parameters[0],
-                kernel_size=parameters[1],
-                stride=parameters[2],
-                padding=parameters[3]
-            ))
-            convs.append(BatchNorm2d(parameters[0])) 
-           
-            if dropout is not None:
+        for parameters in list(
+            zip(out_channel_size, kernel_size, stride, padding_size)
+        ):
+            convs.append(
+                Conv2d(
+                    in_channels=channels,
+                    out_channels=parameters[0],
+                    kernel_size=parameters[1],
+                    stride=parameters[2],
+                    padding=parameters[3],
+                )
+            )
+            convs.append(BatchNorm2d(parameters[0]))
+
+            if dropout2d is not None:
                 convs.append(Dropout2d(dropout2d))
-            
+
             convs.append(ActFunc())
-            
-            height = compute_conv_dim(height, parameters[1], parameters[3], parameters[2])
+
+            height = compute_conv_dim(
+                height, parameters[1], parameters[3], parameters[2]
+            )
             width = compute_conv_dim(width, parameters[1], parameters[3], parameters[2])
             channels = parameters[0]
 
-        convs.append(MaxPool2d(
-                kernel_size = maxpool[0],
-                stride = maxpool[1]
-            ))
-        height = compute_conv_dim(height, maxpool[0], 0, maxpool[1])
-        width = compute_conv_dim(width, maxpool[0], 0, maxpool[1])
+        convs.append(MaxPool2d(kernel_size=maxpool_kernel, stride=maxpool_stride))
+        height = compute_conv_dim(height, maxpool_kernel, 0, maxpool_stride)
+        width = compute_conv_dim(width, maxpool_kernel, 0, maxpool_stride)
 
         self.CNN = Sequential(*convs)
 
-        self.linear_in_features = channels*height*width   
+        self.linear_in_features = channels * height * width
         self.last_im_size = (channels, height, width)
-    
-        
+
         # FEED FORWARD LAYERS -----------------------------------------------------------------
         if ffnn_layer_size is None:
             ffnn_layer_size = [100, 100]
         self._ffnn_layer_size = ffnn_layer_size
-        
+
         layers = []
         in_size = self.linear_in_features
         for out_size in ffnn_layer_size:
@@ -94,35 +104,77 @@ class Encoder(Module):
         self.ffnn = Sequential(*layers)
 
     def forward(self, x):
+
         x = self.CNN(x)
         x = self.ffnn(x.view(-1, self.linear_in_features))
         return x
 
-class Decoder(Module):
 
-    def __init__(self, in_features, reshape_features, out_features, kernel_size = None,
-                 padding_size = None, out_channel_size = None, stride = None,
-                 activation_function = None, ffnn_layer_size = None,
-                 dropout = None, dropout2d = None):
+class Decoder(Module):
+    def __init__(
+        self,
+        in_features,
+        image_size,
+        kernel_size=[2, 3, 2],
+        stride=[2, 2, 1],
+        in_channel_size=[6, 9, 6],
+        activation_function="Tanh",
+        ffnn_layer_size=[100, 1000],
+        dropout=None,
+        dropout2d=None,
+    ):
 
         super().__init__()
 
-        self.reshape_features = reshape_features
-
-        self.dropout = dropout
-        self.dropout2d = dropout2d
-
-        if activation_function is None:
-            activation_function = "ReLU"
-        self._activation_function = activation_function
-
         ActFunc = getattr(torch.nn.modules.activation, activation_function)
+
+        # TRANSPOSED CONVOLUTIONAL LAYERS ------------------------------------------------------
+
+        convs_reversed = []
+        out_channels, out_height, out_width = image_size
+
+        parameters = list(zip(in_channel_size, kernel_size, stride))
+
+        for in_channel_size_, kernel_size_, stride_ in parameters[::-1]:
+
+            if len(convs_reversed) > 0:
+                convs_reversed.append(ActFunc())
+                if dropout2d is not None:
+                    convs_reversed.append(Dropout2d(dropout2d))
+                convs_reversed.append(BatchNorm2d(out_channels))
+
+            M_height = out_height - kernel_size_ + stride_
+            M_width = out_width - kernel_size_ + stride_
+            out_padding_height = M_height % stride_
+            out_padding_width = M_width % stride_
+
+            in_height = (M_height - out_padding_height) // stride_
+            in_width = (M_width - out_padding_width) // stride_
+            
+            conv_layer = ConvTranspose2d(
+                in_channels=in_channel_size_,
+                out_channels=out_channels,
+                kernel_size=kernel_size_,
+                stride=stride_,
+                padding=0,
+                output_padding=(out_padding_height, out_padding_width),
+            )
+
+            convs_reversed.append(conv_layer)
+
+            out_channels = in_channel_size_
+            out_height = in_height
+            out_width = in_width
+
+        convs = convs_reversed[::-1]
+        self.CNN = Sequential(*convs)
+        self._conv_in_shape = (in_channel_size_, out_height, out_width)
 
         # FEED FORWARD LAYERS -----------------------------------------------------------------
         if ffnn_layer_size is None:
             ffnn_layer_size = [100, 100]
         self._ffnn_layer_size = ffnn_layer_size
-        
+
         layers = []
         in_size = in_features
         for out_size in ffnn_layer_size:
@@ -131,71 +183,56 @@ class Decoder(Module):
             if dropout is not None:
                 layers.append(Dropout(dropout))
             in_size = out_size
-        layers.append(Linear(in_size, np.prod(reshape_features)))
+        layers.append(Linear(in_size, np.prod(self._conv_in_shape)))
 
         self.ffnn = Sequential(*layers)
 
-        # TRANSPOSED CONVOLUTIONAL LAYERS ------------------------------------------------------
-        convs = []
-        channels, height, width = self.reshape_features
-        for parameters in list(zip(out_channel_size, kernel_size, stride, padding_size)):
-            convs.append(ConvTranspose2d(
-                in_channels=channels,
-                out_channels=parameters[0],
-                kernel_size=parameters[1],
-                stride=parameters[2],
-                padding=parameters[3]
-            ))
-
-            convs.append(BatchNorm2d(parameters[0])) 
-           
-            if dropout is not None:
-                convs.append(Dropout2d(dropout2d))
-            
-            convs.append(ActFunc())
-
-            height = compute_conv_transpose_dim(height, parameters[1], parameters[3], 1, parameters[2])
-            width = compute_conv_transpose_dim(width, parameters[1], parameters[3], 1, parameters[2])
-            channels = parameters[0]
-
-        # "Manuel" layer to ensure output image size, stride padding and dialation are default sizes
-        k1 = compute_last_kernel_dim(out_dim_size = out_features[1], dim_size = height)
-        k2 = compute_last_kernel_dim(out_dim_size = out_features[2], dim_size = width)
-        kernel_size = (k1,k2)
-
-        convs.append(ConvTranspose2d(
-            in_channels=channels,
-            out_channels=out_features[0]*2,
-            kernel_size=kernel_size,
-        ))
-
-        self.CNN = Sequential(*convs)
-
     def forward(self, x):
+
         x = self.ffnn(x)
-        new_dim = (x.shape[0],) + self.reshape_features
-        x = x.view(new_dim)
+        x = x.view((-1,) + self._conv_in_shape)
         x = self.CNN(x)
+
         return x
+
 
 class CheckZeros(Module):
     def forward(self, x):
         pass
 
+
 if __name__ == "__main__":
     from src.data import SkinCancerDataset
+
     data = SkinCancerDataset(image_size=(225, 300))
     image_size = data.X.shape[1:]
 
-    net = Encoder(image_size, out_features = 4,  kernel_size = [3, 2], padding_size = [2, 1],
-                 out_channel_size = [7, 3], stride = [1,1],
-                 activation_function = None, ffnn_layer_size = None,
-                 dropout = None, dropout2d = None, maxpool = None)
-    
-    hej = Decoder(in_features = 4, reshape_features = net.last_im_size,
-                 out_features = image_size, kernel_size = [3, 2], padding_size =  [2, 1],
-                 out_channel_size = [7, 3], stride = [1,1],
-                 activation_function = None, ffnn_layer_size = None,
-                 dropout = None, dropout2d = None)
+    net = Encoder(
+        image_size,
+        out_features=4,
+        kernel_size=[3, 2],
+        padding_size=[2, 1],
+        out_channel_size=[7, 3],
+        stride=[1, 1],
+        activation_function=None,
+        ffnn_layer_size=None,
+        dropout=None,
+        dropout2d=None,
+        maxpool=None,
+    )
 
-    tmp = hej(net(data.X[0:10,:,:,:]))
+    hej = Decoder(
+        in_features=4,
+        reshape_features=net.last_im_size,
+        out_features=image_size,
+        kernel_size=[3, 2],
+        padding_size=[2, 1],
+        out_channel_size=[7, 3],
+        stride=[1, 1],
+        activation_function=None,
+        ffnn_layer_size=None,
+        dropout=None,
+        dropout2d=None,
+    )
+
+    tmp = hej(net(data.X[0:10, :, :, :]))
--- src/models/vae_cnn.py
+++ src/models/vae_cnn.py
@@ -1,4 +1,4 @@
-# ordinary VAE for the HAM data 
+# ordinary VAE for the HAM data
 from src.models.cnn_encoder_decoder import Decoder, Encoder
 import numpy as np
 import torch
@@ -9,31 +9,42 @@ from torch.distributions.kl import kl_divergence
 
 from torch.utils.data import random_split
 
+
 class ConvolutionalVariationalAutoencoder(nn.Module):
     """A Variational Autoencoder with
     * a Gaussian observation model `p(x|z)`
     * a Gaussian prior `p(z) = N(z | 0, I)`
     * a Gaussian posterior `q(z|x) = N(z | \mu(x), \sigma(x))`
     """
-    def __init__(self, image_size, latent_features):
+
+    def __init__(
+        self, image_size, latent_dim, encoder_params=None, decoder_params=None
+    ):
 
         super().__init__()
 
-        self.latent_features = latent_features
+        self.latent_dim = latent_dim
+        channels, height, width = image_size
 
-        self.encoder = Encoder(image_size, out_features = latent_features*2,  kernel_size = [3, 2], 
-                               padding_size = [1, 1], out_channel_size = [7, 3], stride = [1,1],
-                               activation_function = None, ffnn_layer_size = None,
-                               dropout = None, dropout2d = None, maxpool = None)
-    
-        self.decoder = Decoder(in_features = latent_features,
-                 reshape_features = self.encoder.last_im_size,
-                 out_features = image_size, kernel_size = [3, 2], padding_size =  [1, 1],
-                 out_channel_size = [7, 3], stride = [1,1],
-                 activation_function = None, ffnn_layer_size = [100, 1000],
-                 dropout = None, dropout2d = None)
-        
-        self.register_buffer('prior_params', torch.zeros(torch.Size([1, 2*latent_features])))
+        if encoder_params is None:
+            encoder_params = {}
+
+        self.encoder = Encoder(
+            image_size=image_size, out_features=latent_dim * 2, **encoder_params
+        )
+
+        if decoder_params is None:
+            decoder_params = {}
+
+        self.decoder = Decoder(
+            in_features=latent_dim,
+            image_size=(2 * channels, height, width),
+            **decoder_params
+        )
+
+        self.register_buffer(
+            "prior_params", torch.zeros(torch.Size([1, 2 * latent_dim]))
+        )
 
     def posterior(self, x: Tensor) -> Distribution:
         """return the distribution `q(z|x) = N(z | \mu(x), \sigma(x))`"""
@@ -43,42 +54,44 @@ class ConvolutionalVariationalAutoencoder(nn.Module):
 
         # Return a distribution `q(z|x) = N(z | \mu(x), \sigma(x))`
         return Normal(mu, log_sigma.exp())
-    
-    def prior(self, batch_size: int=1) -> Distribution:
+
+    def prior(self, batch_size: int = 1) -> Distribution:
         """return the distribution `p(z)`"""
-        prior_params = self.prior_params.expand(batch_size, *self.prior_params.shape[-1:])
+        prior_params = self.prior_params.expand(
+            batch_size, *self.prior_params.shape[-1:]
+        )
         mu, log_sigma = prior_params.chunk(2, dim=-1)
 
-        # return the distribution `p(z)` 
+        # return the distribution `p(z)`
         return Normal(mu, log_sigma.exp())
-    
+
     def observation_model(self, z: Tensor) -> Distribution:
         """return the distribution `p(x|z)`"""
         h_z = self.decoder(z)
         mu, log_sigma = h_z.chunk(2, dim=1)
 
-        return Independent(Normal(mu, log_sigma.exp()),3)
+        return Independent(Normal(mu, log_sigma.exp()), 3)
 
     def forward(self, x):
         # define the posterior q(z|x) / encode x into q(z|x)
         qz = self.posterior(x)
-    
+
         # define the prior p(z)
         pz = self.prior(batch_size=x.size(0))
-    
+
         # sample the posterior using the reparameterization trick: z ~ q(z | x)
         z = qz.rsample()
-    
+
         # define the observation model p(x|z) = B(x | g(z))
         px = self.observation_model(z)
-    
-        return {'px': px, 'pz': pz, 'qz': qz, 'z': z}
 
-    def get_loss(self, x, return_kl=False, beta = 1.):
+        return {"px": px, "pz": pz, "qz": qz, "z": z}
+
+    def get_loss(self, x, return_kl=False, beta=1.0):
         output = self(x)
         px, pz, qz, z = [output[k] for k in ["px", "pz", "qz", "z"]]
         kl_term = kl_divergence(Independent(qz, 1), Independent(pz, 1))
-        
+
         loss = -px.log_prob(x) + beta * kl_term
 
         if not return_kl:
@@ -87,31 +100,27 @@ class ConvolutionalVariationalAutoencoder(nn.Module):
             return loss.mean(), kl_term.mean()
 
 
-
-
 if __name__ == "__main__":
-    
+
     torch.manual_seed(123)
     from src.data import SkinCancerDataset
+
     data = SkinCancerDataset(image_size=(225, 300))
     image_size = data.X.shape[1:]
 
-    ham_vae = ConvolutionalVariationalAutoencoder(image_size=image_size, latent_features=3)
+    ham_vae = ConvolutionalVariationalAutoencoder(
+        image_size=image_size, latent_dim=3
+    )
 
-    with torch.autograd.profiler.profile(record_shapes = True) as prof:
-        loss = ham_vae.get_loss(data.X[:10,:,:,:])
+    with torch.autograd.profiler.profile(record_shapes=True) as prof:
+        loss = ham_vae.get_loss(data.X[:10, :, :, :])
+
+    print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     
-    print(prof.key_averages().table(sort_by ="cpu_time_total", row_limit = 10))
-    #print(loss)
-    #loss.backward() 
-    #with torch.no_grad():
+    
+    
+    # print(loss)
+    # loss.backward()
+    # with torch.no_grad():
     #    loss = ham_vae.get_loss(data.X[:10,:,:,:])
     #    print(loss)
-
-
-
-
-
-
-
-
